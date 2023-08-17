8be13cd1338ed02582ac3b6309b4815afaebd748
Paulo Victor <paulovictor.devs@gmail.com>
2023-08-17 18:46:23 -0300

    add solutions to few corner cases that may show up while using

--- script.js
+++ script.js
@@ -2,50 +2,97 @@ const displayResult = document.querySelector('result');
 const buttons = document.querySelectorAll('.btn');
 
 const holder = [];
-const num1 = [];
+const num = [];
 const num2 = [];
 let operator;
 //let result;
 
+const operations = ['add', 'subtract', 'multiply', 'divide'];
+
+
 buttons.forEach((button) => {
-    button.addEventListener('click', (e) => {
- 
-        if(button.value === 'enter') {
-            num1.push(holder.join(''));
-            add(num1[0],num1[1]);
-        }
-        
-
-        else if(button.value === '+' && num1.length === 0) {
-            operator = 'add'
-
-            num1.push(holder.join(''));
-            
-            holder.length = 0;
-
-            console.log('num1:',num1);
-        }
-
-        else if(button.value === '+' && num1.length > 0) {
-            operator = 'add'
-            num1.push(holder.join(''));
-            add(num1[0],num1[1]);
-            
-            holder.length = 0;
-            console.log('num1:',num1);
-        }
-
-        else {
-        console.log('num1length:', num1.length);
-        holder.push(button.value);
-        console.log('holder:',holder);
-
-        }
- })
+    button.addEventListener('click', operate);
 })
 
+
+function operate(event) {
+
+    const button = event.target;
+
+    // This condition is used if the last add item is a operator and not a number.
+    if(button.value === 'enter' && num.length < 2) {
+        
+        num.push(holder.join(''));
+        num.push(0);
+        
+        add(num[0],num[1]);
+
+        holder.length = 0;
+        num.length = 0;
+    }
+
+    else if(button.value === 'enter') {
+        num.push(holder.join(''));
+
+        holder.length = 0;
+
+        add(num[0],num[1]);
+
+        num.length = 0;
+    }
+
+
+    else if(button.value === 'add' && num.length === 0) {
+        operator = 'add'
+
+        num.push(holder.join(''));
+
+        holder.length = 0;
+
+        console.log('num:',num);
+    }
+
+    // This condition is used when user alredy add 2 numbers and keeps adding more operations.
+    else if(button.value === 'add' && num.length > 0) {
+        operator = 'add'
+
+        num.push(holder.join(''));
+
+        console.log(num);
+
+        holder.length = 0;
+        add(num[0],num[1]);
+
+        console.log('num:',num);
+    }
+
+    else {
+    console.log('num:',num);
+    holder.push(button.value);
+    console.log('holder:',holder);
+
+    }
+}
+
+    
 function add(num1,num2) {
+
+    const secondNumber = parseInt(num2);
+
+    if(isNaN(secondNumber)) { // This condition is used to prevent bugs when user keeps pressing operator button.
+        const result = parseInt(num1);
+        num.length = 0;
+        num.push(result);
+
+        console.log('result:',result);
+        return result;
+
+    } else {
     const result = parseInt(num1) + parseInt(num2);
-    console.log(result)
+    num.length = 0;
+    num.push(result);
+
+    console.log(result);
     return result;
+}
 }
